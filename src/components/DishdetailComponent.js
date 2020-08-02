09bb6f978c4d312b582793a1fe05845d9b70acf1
vedanttttt <vatheking@gmail.com>
2020-08-02 09:13:27 +0530

    presentational and container components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -15,9 +15,9 @@ class Dishdetail extends Component {
                     &nbsp;
                     {new Intl.DateTimeFormat('en-US', {
                             year: 'numeric',
-                            month: 'long',
+                            month: 'short',
                             day: '2-digit'
-                        }).format(new Date(comment.date))}
+                        }).format(new Date(Date.parse(comment.date)))}
                     </p>
                 </li>
             )
@@ -60,10 +60,12 @@ class Dishdetail extends Component {
         const dishItem = this.renderDish(dish)
         const commentItem = this.renderComments(dish.comments)
         return (
+          <div className="container">
             <div className='row'>
                 {dishItem}
                 {commentItem}
             </div>
+          </div>  
         )
     }
 }
