f0b2f8a91bd5511ccf5b71d02cbbb3899efcf39c
vedanttttt <vatheking@gmail.com>
2020-08-06 17:17:34 +0530

    single page applictions part-2

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {Breadcrumb,BreadcrumbItem} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 function Contact(props) {
     return(
         <div className="container">
+            <div className="row">
+                  <Breadcrumb>
+                    <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+                  </Breadcrumb>
+                  <div className="col-12">
+                    <h3>Contact Us</h3>
+                    <hr />
+                  </div>  
+            </div>
             <div className="row row-content">
                 <div className="col-12">
                 <h3>Location Information</h3>
@@ -17,7 +29,7 @@ function Contact(props) {
                         <i className="fa fa-fax"></i>: +852 8765 4321<br />
                         <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
                         </address>
-                </div>
+            </div>
                 <div className="col-12 col-sm-6 offset-sm-1">
                     <h5>Map of our Location</h5>
                 </div>
